935c00c856482c0c494488476c7ae7286e26cc85
Keys <otegami@devel.keys.jp>
2019-09-17 16:52:04 +0900

    update

--- scraping.py
+++ scraping.py
@@ -1,57 +1,55 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
-def get_data():
-    pass
 
 def print_result(items):
-    rank_level_text = {
-        'type2': 'スッキリす',
-        'type3': 'まあまあスッキリす',
-    }
     for month in items:
         if items[month]['rank_level'] == 'type1':
             rank = '超スッキリす！'
         elif items[month]['rank_level'] == 'type4':
             rank = 'ガッカリす...'
         else:
-            rank = '{} {}'.format(
-                items[month]['rank'],
-                rank_level_text[items[month]['rank_level']]
-            )
-        print('{} : {}月 : {}。ラッキーカラーは{}。'.format(
+            if items[month]['rank_level'] == 'type2':
+                rank = 'スッキリす'
+            elif items[month]['rank_level'] == 'type3':
+                rank = 'まあまあスッキリす'
+            rank = '{} {}'.format(items[month]['rank'], rank)
+        print('{} : {}月 {}。ラッキーカラーは{}。'.format(
             rank,
             month,
             items[month]['text'],
             items[month]['color']
         ))
 
-def scraping():
-    # url = "http://www.ntv.co.jp/sukkiri/sukkirisu/index.html"
-    # html = request.urlopen(url)
-    # soup = BeautifulSoup(html, "html.parser")
-    soup = BeautifulSoup(open("tmp/sukkirisu.html", encoding='utf8'))
-
+def get_sukkirisu(soup):
     items = dict()
-    for part in soup.find_all("div", class_='flTxt'):
-        message_text = part.find_all('p')
-        rank_level = part.attrs['class'][0]
-        if len(message_text) == 3:
-            month = message_text[0].string
-            rank = message_text[1].string
-            message = message_text[2].string
-        elif len(message_text) == 2:
-            month = message_text[0].string
+    for div_body in soup.find_all("div", class_='flTxt'):
+        part = div_body.find_all('p')
+        rank_level = div_body.attrs['class'][0]
+        if len(part) == 3:
+            month = part[0].string
+            rank = part[1].string
+            message = part[2].string
+        elif len(part) == 2:
+            month = part[0].string
             rank = ''
-            message = message_text[1].string
+            message = part[1].string
         item = {
             'rank': rank,
             'rank_level': rank_level,
             'text': message,
-            'color': part.find(id='color').string
+            'color': div_body.find(id='color').string
         }
-        month = part.find("p", class_="monthTxt").string
+        month = div_body.find("p", class_="monthTxt").string
         items[month] = item
+    return items
+
+def scraping():
+    # url = "http://www.ntv.co.jp/sukkiri/sukkirisu/index.html"
+    # html = request.urlopen(url)
+    # soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(open("tmp/sukkirisu.html", encoding='utf8'), features='html.parser')
+    items = get_sukkirisu(soup)
     print_result(items)
 
 if __name__ == "__main__":
